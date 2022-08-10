6e045c51c582f59395cffa91e501a87cb9d57f83
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-10 15:42:50 +0530

    Promise

--- 10th_Aug_Promise.js
+++ 10th_Aug_Promise.js
@@ -4,7 +4,7 @@
 // console.log("before")
 // setTimeout (()=>{
 // console.log("reading data")
-// return {id:244 ,githubUserName:Rajesh}
+// return {id:244 ,githubUserName:"Rajesh"}
 // },2000)
 // console.log("after")
 //1.before 2. after 3. reading data
@@ -26,17 +26,43 @@
 //will print "after"  after that it will print "reading data"
 
 // but problem is that if we want to wait here we use  callBack function 
-function getData(id,callback){
-    setTimeout (()=>{
-    console.log("reading data")
-    let user= ({id:id ,githubUserName:"Rajesh"})
-    console.log(user)
-    },2000)
-    }
-    console.log("before")
-    const user = getData(244,function callback (user){
-        console.log(user)
-    })
-    console.log("after")
+// function getData(id,callback){
+//     setTimeout (()=>{
+//     console.log("reading data")
+//     let user= ({id:id ,githubUserName:"Rajesh"})
+//     console.log(user)
+//     },2000)
+//     }
+//     console.log("before")
+//     const user = getData(244,function callback (user){
+//         console.log(user)
+//     })
+//     console.log("after")
     //getData is HOF and function is call back function 
-   // 
+   // now it is also synchronus because it not wait still 
+   //let's create a scenario 
+   // hotels food ordering process
+   //process => entering hotel->oder food->food is prepared -> delivered food-> eating-> payment
+function foodPrepared (startEating){
+    setTimeout (()=>{
+        console.log("food getting  prepared")
+        console.log("delivered food")
+        startEating()
+    },1000)
+}
+
+function eatingFood (){
+    setTimeout (()=>{
+        console.log("eating")
+        console.log("payment")
+    },5000)
+}
+
+   function hotelVisit (){
+   console.log("entering hotel")
+   console.log("oder food")
+   foodPrepared(eatingFood)//foodPrepared is HOF and eatingFood is callBack function
+   
+  
+}
+hotelVisit()
