2ea2f256f53602b23e5e43609df11262784426c8
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-18 19:10:23 +0530

    Promises

--- 18Aug_Promises.js
+++ 18Aug_Promises.js
@@ -1,11 +1,28 @@
-let Promise = new Promise(function(Resolve, Reject) {
-    let x = 0;
-    if (x == 0) {
-      Resolve("OK");
-    } else {
-      Reject("Error");
-    }
-  });
-   Promise.then((result) => console.log(result))
-   .catch((error) => console.log(error));
+// let Promise = new Promise(function(Resolve, Reject) {
+//     let x = 0;
+//     if (x == 0) {
+//       Resolve("OK");
+//     } else {
+//       Reject("Error");
+//     }
+//   });
+//    Promise .then((result) => console.log(result))
+//    .catch((error) => console.log(error));
+
+//    const promise1 = new Promise((resolve, reject) => {
+//     resolve('Success!');
+//   });
   
+//   promise1.then((value) => {
+//     console.log(value);
+//   })
+const Promise2 = new Promise ((resolve,reject)=>{
+resolve("successful")
+reject("unsuccessful")
+})
+Promise2.then ((value)=>{
+console.log(value)
+})
+Promise2.catch ((value)=>{
+console.log(value)
+}) 
