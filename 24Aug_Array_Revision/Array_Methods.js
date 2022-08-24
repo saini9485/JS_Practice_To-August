81f9ae4b58e030194ea70764864a3d17965bb70f
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-24 10:06:14 +0530

    Array Methods

---
+++ 24Aug_Array_Revision/Array_Methods.js
@@ -0,0 +1,50 @@
+/*in javascript array is a object  where can store multiple value with single variable
+ */
+// Array declearation
+method: -1;
+// let arr = [1,2,3,4,5]
+
+method: -2; //with new keyword
+
+// let arr = new Array ["Rajesh", "saini"]
+
+//Array Methods :-
+// let arr = [1, 2, 3, 4, 5, 6, 8, 9];
+// ForEach => it will itetarate all value and will return according to given consition
+methods:-1
+// arr.forEach((num) => {
+//   return num;
+// });
+// console.log(arr);
+
+Methods:-2 //includes:- it check the value whether present or not 
+// let arr = [1, 2, 3, 4, 5, 6, 8, 9];
+// let check = arr.includes(5)
+// console.log(check)
+
+method:-3 //length => it will check array length
+// let arr = [1, 2, 3, 4, 5, 9, 6, 8];
+// console.log(arr.length);
+
+methods:-4 // sorting decending order
+let arr = [1, 2, 3, 4, 5, 9, 6, 8];
+// arr.sort (function (a,b){
+// return (b-a)
+// });
+// console.log(arr)
+
+methods:-5 // reverse 
+// let reverse = arr.reverse()
+// console.log(reverse)
+
+method:-6 //shift 
+// let Delete = arr.shift(4)
+// console.log(Delete ,6)
+
+method:-6//unshift 
+//  arr.unshift(6)
+// console.log(arr)
+
+method:-7 //slice 
+let newArr = arr.slice(3,5)
+console.log(newArr)
